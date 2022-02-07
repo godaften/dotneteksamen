ab18e058e779dc57c749897dcdc990fbc03544b5
ckirschberg <ckirschberg@gmail.com>
2022-02-07 10:56:56 +0100

    .

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using cbsStudents.Models.Entities;
 
 namespace cbsStudents.Controllers;
 
@@ -7,6 +8,13 @@ public class PostsController : Controller
     public IActionResult Create()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create([Bind("Title", "Text", "Status")] Post post)
+    {
+        Console.WriteLine(post.Text + " " + post.Title);
+        return RedirectToAction("Test");
     }
 
 
