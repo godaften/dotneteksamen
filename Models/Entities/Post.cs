40cbccd44551f2a06a4261607ea70db6c0163dd1
ckirschberg <ckirschberg@gmail.com>
2022-02-07 11:48:37 +0100

    .

--- Models/Entities/Post.cs
+++ Models/Entities/Post.cs
@@ -7,8 +7,12 @@ public class Post
     public int Id { get; set; }
 
 
+    [Required(ErrorMessage = "Please fill out title")]
+    [MinLength(3, ErrorMessage = "Minimum 3 characters")]
     public string? Title { get; set; }
 
+    [Required]
+    [MinLength(10)]
     public string? Text { get; set; }
 
     [DataType(DataType.Date)]
